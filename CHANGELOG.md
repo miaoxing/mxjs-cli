180dfb2ebba32aa468a54ea706ab35aa03f8a38c
twinh <twinhuang@qq.com>
2020-07-31 15:08:46 +0800

    refactor: 更新包名和目录，避免前后端冲突

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 0.1.0 (2020-07-30)
+
+
+### Features
+
+* 增加 watch-doc 命令，监听目录更改自动生成文档 ([](https://github.com/miaoxing/mxjs-cli/commit/))
---
+++ index.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env ts-node
+
+import * as yargs from 'yargs';
+import * as npmlog from 'npmlog';
+import * as globby from 'globby';
+
+(async () => {
+  const paths = await globby('plugins/*/commands/*');
+  paths.forEach(path => {
+    yargs.command(require(process.cwd() + '/' + path).default);
+  });
+
+  yargs
+    .options({
+      'log-level': {
+        group: 'Global Options:',
+        describe: 'The level to display logs at',
+        type: 'string',
+        default: 'info',
+      },
+    })
+    .middleware([
+      argv => {
+        // @ts-ignore
+        npmlog.level = argv.logLevel;
+      },
+    ])
+    .strict()
+    .help()
+    .argv;
+})();
